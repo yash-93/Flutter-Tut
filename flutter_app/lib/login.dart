e89cde0ab5b1cbfcf409c7f340d5a1db5e9d90da
yash-93 <yashdeepbachhas93@gmail.com>
2020-08-28 19:40:35 +0530

    created components for each class and used Navigator

---
+++ flutter_app/lib/login.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/myHomePage.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Hello World")), body: Body());
+  }
+}
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  String name;
+  TextEditingController controller = new TextEditingController();
+
+  void click() {
+    this.name = controller.text;
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => MyHomePage(this.name)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: TextField(
+            controller: this.controller,
+            decoration: InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                labelText: "Type your name...",
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 5, color: Colors.black)),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.done),
+                  splashColor: Colors.blueAccent,
+                  tooltip: "Submit",
+                  onPressed: this.click,
+                )),
+          ),
+        ));
+  }
+}
---
+++ flutter_app/lib/myHomePage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'post.dart';
+import 'postList.dart';
+import 'textInputWidget.dart';
+
+class MyHomePage extends StatefulWidget {
+  final String name;
+
+  MyHomePage(this.name);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Post> posts = [];
+
+  void newPost(String text) {
+    this.setState(() {
+      posts.add(new Post(text, widget.name));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Hello World")),
+        body: Column(children: <Widget>[
+          Expanded(child: PostList(this.posts)),
+          TextInputWidget(this.newPost)
+        ]));
+  }
+}
